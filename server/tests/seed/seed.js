5529e6ea6b15b5580118f2f4d29711e98fe530dd
Connor Chan <cchan2@conncoll.edu>
2018-03-25 12:59:04 -0400

    Make routes private and revise tests for Udemy course

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
   _id: userTwoId,
   email: 'cjc@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }]
 
 const todos = [{
   _id: new ObjectID(),
-  text: "Go to work"
+  text: "Go to work",
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: "Make dinner",
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
